105e1792a4b6c3d5596259a6c3fc45c0dc32a64a
pankaj <pkp20022@gmail.com>
2024-04-10 19:42:23 +0530

    Recommended Card done

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -6,11 +6,12 @@ import "./Location.css";
 function Location({
     location, 
     style,
+    size="",
     nextBtn=false,
 }){
     return(
-        <div className="location-container" style={style}>
-            <IoLocationSharp className="location-icon"/>
+        <div className="location-container" style={{fontSize:size}}>
+            <IoLocationSharp className="location-icon" style={style}/>
             {location}
             {nextBtn && <MdNavigateNext className="next-icon"/>}
         </div>
--- src/components/RecommendedCard/RecommendedCard.jsx
+++ src/components/RecommendedCard/RecommendedCard.jsx
@@ -4,13 +4,16 @@ import {Location} from "../index.js"
 function RecommendedCard(props) {
   return (
     <div className = "card">
-        <img src="https://drive.google.com/thumbnail?id=1lS1XAo47YvNSoFp1NE5rmhTSQ8qNBWEh" alt="Image Description"/>
+        <img src="https://drive.google.com/thumbnail?id=1DJDmufd4dZw--32HpuAxNSfPJqqHa6Pq" alt="Image Description"/>
         <div className="card-content">
-            <h2>
-                Card Heading
-            </h2>
-            <Location className="location" locatioin={"Madhya Pradesh, India"}/>
-            
+          <div className="card-item">
+            <span>Make Agree</span>
+            <p className="card-date">March 23, 2024</p>
+          </div>
+          <div className="card-item second-item">
+            <Location  location={"West Dougals"} style={{marginRight: ".3rem"}} size={".6rem"}/>
+            <p>Snowy, 26*C | 42 Km</p>
+          </div>  
         </div>
   </div>
   )
