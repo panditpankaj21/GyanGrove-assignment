d6733f98b1d0775b83c6f9a0faaba00f988efa69
pankaj <pkp20022@gmail.com>
2024-04-19 08:45:12 +0530

    added pagination

--- src/components/UpcomingCard.jsx
+++ src/components/UpcomingCard.jsx
@@ -1,20 +1,22 @@
 import {Location} from "./index.js"
 
-function UpcomingCard(){
+function UpcomingCard({item}){
+    const {eventName, cityName, imgUrl, weather, distanceKm} = item;
+    const imageId = imgUrl.split('/')[5]
     return(
         <div className="border-[#B0BABF] border-[1px] w-80 rounded-lg overflow-hidden ">
             <div className="relative">
                 <img 
-                    src="https://drive.google.com/thumbnail?id=1p3UpSZMEVhQRKyqlxbvl4X-rNCWXmqF_" alt="image" 
+                    src={`https://drive.google.com/thumbnail?id=${imageId}`} alt="image" 
                     className="w-full"
                 />
                 <p className="absolute bottom-[15px] ml-[12px] w-[293px] bg-black text-white bg-opacity-70 rounded-b-lg pl-3 text-xs py-1">March 23, 2024</p>
             </div>
             <div className="ml-3 "> 
-                <h4 className="font-extrabold text-sm pb-1">After note nearly</h4>
+                <h4 className="font-extrabold text-sm pb-1">{eventName}</h4>
                 <div className="flex justify-between pr-3 text-[#B0BABF] pb-3">
-                    <Location location={"West Dougles"}/>
-                    <p className="text-xs">Snowy, 26*C | 42 Km</p>
+                    <Location location={cityName}/>
+                    <p className="text-xs">{weather} | {Math.round(distanceKm).toLocaleString()} Km</p>
                 </div>
             </div>
             
--- src/pages/UpComingShow.jsx
+++ src/pages/UpComingShow.jsx
@@ -1,26 +1,87 @@
-import { UpcomingCard } from "../components/index.js";
-import { GrFormNextLink } from "react-icons/gr";
+import React, { useState, useEffect, useRef } from 'react';
+import { UpcomingCard } from '../components/index.js';
+import { GrFormNextLink } from 'react-icons/gr';
 
-function UpComingShow(){
-    const content = [<UpcomingCard/>, <UpcomingCard/>, <UpcomingCard/>, <UpcomingCard/>, <UpcomingCard/>, <UpcomingCard/>, <UpcomingCard/>,<UpcomingCard/>, <UpcomingCard/>]
-    return(
+function UpComingShow() {
+    const [content, setContent] = useState([]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [hasMore, setHasMore] = useState(true);
+    const observer = useRef();
+    const lastElementRef = useRef();
+
+    useEffect(() => {
+        async function fetchEvents() {
+            setLoading(true);
+            try {
+                const response = await fetch(`https://gg-backend-assignment.azurewebsites.net/api/Events?code=FOX643kbHEAkyPbdd8nwNLkekHcL4z0hzWBGCd64Ur7mAzFuRCHeyQ==&page=${page}&type=upcoming`);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                const data = await response.json();
+                setContent(prevContent => [...prevContent, ...data.events]);
+                if(data.totalPages === page){
+                    setHasMore(false);
+                }else{
+                    setHasMore(true);
+                }
+            } catch (error) {
+                console.error('There was a problem with your fetch operation:', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        if(hasMore){
+            fetchEvents();
+        }
+    }, [page]);
+
+    useEffect(() => {
+        if (loading) return;
+
+        if (observer.current) observer.current.disconnect();
+
+        const callback = entries => {
+            if (entries[0].isIntersecting && hasMore) {
+                setPage(prev => prev + 1);
+            }
+        };
+
+        observer.current = new IntersectionObserver(callback);
+        if (lastElementRef.current) {
+            observer.current.observe(lastElementRef.current);
+        }
+
+        return () => {
+            if (observer.current) {
+                observer.current.disconnect();
+            }
+        };
+    }, [loading, hasMore]);
+
+    return (
         <div className="sm:mx-14 mt-5 sm:mr-40">
             <div className="flex justify-around sm:justify-between">
                 <div className="flex items-center mb-1">
                     <p>Upcoming events</p>
                     <GrFormNextLink className="text-xl ml-1" />
                 </div>
-                <p className="underline">see all</p>
+                <p className="underline cursor-pointer">see all</p>
             </div>
             <div className="sm:grid flex flex-col gap-10 items-center sm:grid-cols-3 sm:gap-y-7">
-                {
-                    content.map((item, index) => (
-                        <div key={index}>{item}</div>
-                    ))
-                }
+                {content.map((item, index) => {
+                    if (index === content.length - 1) {
+                        return <div ref={lastElementRef} key={index}><UpcomingCard item={item} /></div>;
+                    } else {
+                        return <div key={index}><UpcomingCard item={item} /></div>;
+                    }
+                })}
             </div>
+            {loading && <div className='flex items-center justify-center p-3'>
+                <div className="spinner"></div>
+             </div>}
         </div>
-    )
+    );
 }
 
 export default UpComingShow;
